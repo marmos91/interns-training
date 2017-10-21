7b67ee1356bdfd374d18e1eabca6054ce938ca63
Marco Moschettini <m.marmos@gmail.com>
2017-10-21 21:16:28 +0200

    Added test file

---
+++ test/index.js
@@ -0,0 +1,15 @@
+const hello = require('../src/index.js');
+const expect = require('chai').expect;
+
+describe('level0', function ()
+{
+   it('should say hello world with no parameters', function()
+   {
+       expect(hello.say()).to.be.equal('Hello world!');
+   });
+
+    it('should say hello ${name} when a parameter is specified', function()
+    {
+        expect(hello.say('Marco')).to.be.equal('Hello Marco!');
+    });
+});
