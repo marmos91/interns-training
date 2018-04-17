cf354ab2910a7722873333a0a4be0537504b204c
allefossa9663 <alessandro.fossa2@studio.unibo.it>
2018-04-17 10:47:54 +0200

    level 2 modified

--- test/Calculator.ts
+++ test/Calculator.ts
@@ -8,7 +8,7 @@ describe('Level 2.1 - Calculator Test', () =>
 
     beforeEach(() => calculator = new Calculator());
 
-    it('should return the result of the sum ', async () =>
+    it('should return the result of the sum', async () =>
     {
         calculator.input(3);
         calculator.operation(Operation.ADD);
@@ -16,35 +16,41 @@ describe('Level 2.1 - Calculator Test', () =>
         expect(await calculator.result).to.equal(8);
     });
 
-    it('should return the result of the multiplication ',async () => {
+    it('should return the result of the multiplication',async () =>
+    {
         calculator.input(3);
         calculator.operation(Operation.MUL);
         calculator.input(5);
         expect(await calculator.result).to.equal(15);
     });
 
-    it('should return the result of the subtraction ',async () => {
+    it('should return the result of the subtraction', async () =>
+    {
         calculator.input(3);
         calculator.operation(Operation.SUB);
         calculator.input(5);
         expect(await calculator.result).to.equal(-2);
     });
 
-    it('should return the result of the division ',async () => {
+    it('should return the result of the division', async () =>
+    {
         calculator.input(25);
         calculator.operation(Operation.DIV);
         calculator.input(5);
         expect(await calculator.result).to.equal(5);
     });
 
-    it('should throw exception if trying to divide by 0',async()=>{
+    it('should throw exception if trying to divide by 0', async () =>
+    {
        calculator.input(25);
        calculator.operation(Operation.DIV);
-       expect(()=> calculator.input(0)).throw();
+       expect(() => calculator.input(0)).throw();
     });
 
-    it('should throw exception if an operation is pending',async() =>{
+    it('should throw exception if an operation is pending', async () =>
+    {
         calculator.operation(Operation.DIV);
         expect(() => calculator.operation(Operation.MUL)).throw();
     });
+
 });
--- test/Webpage.ts
+++ test/Webpage.ts
@@ -5,25 +5,31 @@ import * as fs from 'fs';
 const webpage = new Webpage();
 var ServerMock = require("mock-http-server");
 
-describe('Level 2.2 - Webpage Test', ()=>{
+describe('Level 2.2 - Webpage Test', () =>
+{
 
-    var server = new ServerMock({ host: "localhost", port: 9000 });
+    var server = new ServerMock({host: "localhost", port: 9000});
 
-    beforeEach(function(done) {
+    beforeEach(function(done)
+    {
         server.start(done);
     });
 
-    afterEach(function(done) {
+    afterEach(function(done)
+    {
         server.stop(done);
     });
 
-    it('Should return the body of the specified url',async() => {
-        server.on({
+    it('Should return the body of the specified url', async () =>
+    {
+        server.on
+        ({
             method: 'GET',
             path: '/resource',
-            reply: {
+            reply:
+                {
                 status:  200,
-                headers: { "content-type": "application/json" },
+                headers: {"content-type": "application/json"},
                 body: 'esempio'
             }
         });
@@ -32,42 +38,50 @@ describe('Level 2.2 - Webpage Test', ()=>{
         expect(page).to.equal('esempio');
     });
 
-    it('Should retrieve a webpage and save the content to a file', async() =>{
+    it('Should retrieve a webpage and save the content to a file', async () =>
+    {
 
-        server.on({
+        server.on
+        ({
             method: 'GET',
             path: '/resource',
-            reply: {
+            reply:
+                {
                 status:  200,
-                headers: { "content-type": "application/json" },
+                headers: {"content-type": "application/json"},
                 body: 'esempio'
             }
         });
 
-        try{
-            const page = await webpage.saveWebpage('http://localhost:9000/resource','/home/alessandro/Desktop/interns-training/test.txt');
-            const content = fs.readFileSync(page,'utf8')
+        try
+        {
+            const page = await webpage.saveWebpage('http://localhost:9000/resource', '/home/alessandro/Desktop/interns-training/test.txt');
+            const content = fs.readFileSync(page, 'utf8');
             expect(content).to.equal('esempio');
         }
-        catch(error){
+        catch(error)
+        {
             console.log(error);
             expect(0).to.equal(1);  //non sapevo come gestire il catch quindi ho messo questa linea per fare risultare sempre negativo in caso di errore
         }
         });
 
-    it('Should throw an error if something went wrong getting the webpage', () =>{
+    it('Should throw an error if something went wrong getting the webpage', () =>
+    {
 
-        server.on({
+        server.on
+        ({
             method: 'GET',
             path: '/resource',
-            reply: {
+            reply:
+                {
                 status:  200,
-                headers: { "content-type": "application/json" },
+                headers: {"content-type": "application/json"},
                 body: 'esempio'
             }
         });
 
-        return webpage.saveWebpage('http://localhost:9000/abc','/home/alessandro/Desktop/interns-training/test.txt').catch(err => assert.isDefined(err));
+        return webpage.saveWebpage('http://localhost:9000/abc', '/home/alessandro/Desktop/interns-training/test.txt').catch(err => assert.isDefined(err));
 
     });
 
