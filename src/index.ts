d68119b6ee4e55011ebc6906005a861b881473ca
allefossa9663 <alessandro.fossa2@studio.unibo.it>
2018-04-17 10:23:18 +0200

    level 1 modified

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-export class Challenge {
-    buzz (numero : number) {
-        const result = new Promise(function(resolve, reject){
-            if(numero % 3 == 0 && numero % 5 == 0 )
-                resolve('fizzbuzz');
-            else if(numero % 3 == 0)
-                resolve('fizz');
-            else if( numero % 5 == 0)
-                resolve('buzz');
+export class Challenge
+{
+    buzz (entry: number): Promise<any>
+    {
+        return new Promise(function(resolve, reject)
+        {
+            if(entry % 3 == 0 && entry % 5 == 0)
+                return resolve('fizzbuzz');
+            else if(entry % 3 == 0)
+                return resolve('fizz');
+            else if(entry % 5 == 0)
+                return resolve('buzz');
             else
-                resolve(numero);
+                return resolve(entry);
         });
-        return result;
     }
 }
