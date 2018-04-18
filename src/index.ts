c3ca98ba5552bc2152c622854fa2dbc4c038bdf2
Jes <jes.alpha@gmail.com>
2018-04-19 00:12:51 +0200

    corrected revised code for level1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 export class Challenge
 {
-    num: number;
-
-    constructor()
-    {
-    }
-
     public buzz(i: number)
     {
         return new Promise(function (resolve, reject)
         {
             if (i % 3 == 0 && i % 5 == 0)
-                resolve("fizzbuzz");
-            if (i % 3 == 0)
-                resolve("fizz");
-            if (i % 5 == 0)
-                resolve("buzz");
+                return resolve("fizzbuzz");
+            else if (i % 3 == 0)
+                return resolve("fizz");
+            else if (i % 5 == 0)
+                return resolve("buzz");
             else
                 resolve(i);
         });
