173c9da257ae8f40bf9599ae7d2671814b8002a6
Jes <jes.alpha@gmail.com>
2018-04-18 22:52:31 +0200

    adjusted indentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-export class Challenge {
-    num : number;
-    constructor(){
+export class Challenge
+{
+    num: number;
+
+    constructor()
+    {
     }
-    public buzz(i:number){
-        return new Promise(function(resolve, reject){
-            if(i%3==0 && i%5==0)
+
+    public buzz(i: number)
+    {
+        return new Promise(function (resolve, reject)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
                 resolve("fizzbuzz");
-            if(i%3==0)
+            if (i % 3 == 0)
                 resolve("fizz");
-            if(i%5==0)
+            if (i % 5 == 0)
                 resolve("buzz");
             else
                 resolve(i);
