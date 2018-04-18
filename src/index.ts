bdd6447c4ca05cf49366f14a9dd8fe338420eccf
Jes <jes.alpha@gmail.com>
2018-04-18 22:42:04 +0200

    solved level1-hello typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export class Challenge {
+    num : number;
+    constructor(){
+    }
+    public buzz(i:number){
+        return new Promise(function(resolve, reject){
+            if(i%3==0 && i%5==0)
+                resolve("fizzbuzz");
+            if(i%3==0)
+                resolve("fizz");
+            if(i%5==0)
+                resolve("buzz");
+            else
+                resolve(i);
+        });
+    }
+}
