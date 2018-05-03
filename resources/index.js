d09b66718c320152b3dda18f82ee640907850fec
allefossa9663 <alessandro.fossa2@studio.unibo.it>
2018-05-03 11:05:21 +0200

    level 4 completed

--- resources/index.js
+++ resources/index.js
@@ -1,26 +1,38 @@
-var electron = require("electron");
+const electron = require('electron');
+const {ipcRenderer} = electron;
 let p = getRandom(0, 65536);
+ipcRenderer.send('async-port',p);
 document.querySelector('#send').addEventListener("click", bottoneCliccato);
 document.querySelector('#porta').innerHTML = `<p>Server is listening on port ${p}</p>`;
 
+/**
+ * this function collect the value of ip,port and file and sends them to the main
+ * process when the button is clicked
+ */
 function bottoneCliccato()
 {
+    let file = document.querySelector('#inputId').files[0].path;
     let ip = document.querySelector('.Ip').value;
     let port = document.querySelector('.port').value;
-    if (ip && port && port < 65536 && port > 0)
+    if (ip && port && port < 65536 && port > 0 && file)
     {
-        document.querySelector('#output').innerHTML = `<p>Invio a indirizzo ip=${ip} port=${port} </p>`;
-        electron.ipcRenderer.sendSync(ip);
+        document.querySelector('#output').innerHTML = `<p>Sending file to ${ip}:${port}</p>`;
+        ipcRenderer.send('async', ip, port, file);
     }
     else
     {
         document.querySelector('#output').innerHTML = `
-        <article class="message is-danger">
-        <p>Seleziona una porta e un ip address validi</p>
+        <article class="message">
+        <p>fill the form</p>
         </article>`;
     }
 }
 
+/**This function generate a random number between two extremes (which are included)
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ */
 function getRandom(min, max)
 {
     min = Math.ceil(min);
--- src/App.ts
+++ src/App.ts
@@ -1,21 +1,91 @@
 import * as net from 'net';
+const fs = require('fs');
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
 
+
 export class App
 {
-    public server: net.Server;
 
     public listen()
     {
-        this.server = net.createServer();
-        electron.app.on('ready',this.createWindow);
-        electron.ipcMain.on('asynchronous-message', (event, arg) => {
-            console.log(arg)  // prints ip spero
-          });
-    }
+        let num: number = 0;
+        let path: string = './Downloads';
+        var exist = this.statPath(path);
+        if(!exist)
+        {
+            fs.mkdir(path);
+        }
+        electron.app.on('ready', this.createWindow);
 
+        electron.ipcMain.on('async-port', (event, port) =>
+        {
+            const server = net.createServer((socket) =>
+            {
+
+                socket.on('end', () =>
+                {
+                    console.log('client disconnected');
+                });
+
+                socket.on('data',(data) =>
+                {
+                    console.log('Receiving data\nTransfered Bytes : ' + data.length);
+                    let fileToSave = path + '/file' + num;
+                    num++;
+                    console.log(fileToSave);
+                    fs.open(fileToSave, "w", function (err, fd) 
+                    {
+                        fs.writeFile(fileToSave, data, (err) =>
+                        {
+                            if (err) throw err;
+                            console.log('The file has been saved!');
+                        });
+                    });
+                });
+            });
+
+            server.listen(port, () =>
+            {
+                console.log('server is listening on port ' + port);
+            });
+
+            server.on('error', (err) =>
+            {
+                throw err;
+            });
+    
+            server.on('connection',() =>
+            {
+                console.log('Connection to client established');
+            });
+
+        });
+
+        electron.ipcMain.on('async', (event, ip, port, file) =>
+        {
+            var socket = new net.Socket;
+            var fileToSave = file.substring(file.lastIndexOf("/"));
+            console.log(fileToSave);
+            fs.readFile(file, "utf8", (err, data) =>
+            {
+                if(!err)
+                {
+                    socket.connect(port, ip);
+                    socket.write(data, () =>
+                    {
+                      console.log('Sending file');
+                    });
+                }
+                else
+                {
+                  console.log('readfile ' + file + ' err');
+                }
+            });
+
+        });
+    }
 
     /**
      * This method create the windows and load index.html
@@ -29,9 +99,20 @@ export class App
             protocol: 'file:',
             slashes: true
         }));
-        win.webContents.openDevTools();
-        win.on('closed', function(){
+        //win.webContents.openDevTools();
+        win.on('closed', function()
+        {
             electron.app.quit();
         });
     }
+
+    private statPath(path)
+    {
+        try 
+        {
+          return fs.statSync(path);
+        }
+        catch (ex) {}
+        return false;
+      }
 }
