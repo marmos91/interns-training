f7d87ec2ed5f7b33bf6535765b8c0f095e5ea632
Francesco Pigozzi <pigozzi.fr@gmail.com>
2019-10-08 17:06:42 +0200

    Modify indentation

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,78 +1,78 @@
 export enum Operation
 {
-    ADD = 1,
-    SUB,
-    MUL,
-    DIV
+  ADD = 1,
+  SUB,
+  MUL,
+  DIV
 }
 
 export class Calculator
 {
-    private _value;
-    private _operation;
+  private _value;
+  private _operation;
 
-    public constructor()
+  public constructor()
+  {
+    this._value = 0;
+    this._operation = undefined;
+  }
+
+  /**
+   * Set the new state accoding to operations.
+   * @param value Number in input.
+   * @throws <i>Error</i> if trying to divide by 0.
+   */
+  public input(value: number)
+  {
+    if (!this._operation)
     {
-        this._value = 0;
-        this._operation = undefined;
+      this._value = value;
+      return;
     }
 
-    /**
-     * Set the new state accoding to operations.
-     * @param value Number in input.
-     * @throws <i>Error</i> if trying to divide by 0.
-     */
-    public input(value: number)
+    switch (this._operation)
     {
-        if (!this._operation)
-        {
-            this._value = value;
-            return;
-        }
-
-        switch (this._operation)
-        {
-            case Operation.ADD:
-                this._value += value;
-                break;
-            case Operation.SUB:
-                this._value -= value;
-                break;
-            case Operation.MUL:
-                this._value *= value;
-                break;
-            case Operation.DIV:
-                if (value === 0)
-                    throw new Error('Cannot divide by 0. Insert a different value.')
-                this._value /= value;
-                break;
-            default:
-                throw new Error('Unknown operation.') //NOTE Should never happens.
-        }
-
-        this._operation = undefined;
+      case Operation.ADD:
+        this._value += value;
+        break;
+      case Operation.SUB:
+        this._value -= value;
+        break;
+      case Operation.MUL:
+        this._value *= value;
+        break;
+      case Operation.DIV:
+        if (value === 0)
+          throw new Error('Cannot divide by 0. Insert a different value.')
+        this._value /= value;
+        break;
+      default:
+        throw new Error('Unknown operation.') //NOTE Should never happens.
     }
 
-    /**
-     * Set the next operation.
-     * @param type Operation to perform.
-     * @throws <i>Error</i> if an operation is already pending.
-     */
-    public operation(type: Operation)
-    {
-        if (this._operation && this._operation !== type)
-            throw new Error('Pending operation... Insert a value.')
-        this._operation = type;
-    }
+    this._operation = undefined;
+  }
 
-    /**
-     * Get the result.
-     * @throws <i>Error</i> if an operation is pending.
-     */
-    public get result(): number
-    {
-        if (this._operation)
-            throw new Error('Pending operation... Insert a value.')
-        return this._value;
-    }
+  /**
+   * Set the next operation.
+   * @param type Operation to perform.
+   * @throws <i>Error</i> if an operation is already pending.
+   */
+  public operation(type: Operation)
+  {
+    if (this._operation && this._operation !== type)
+      throw new Error('Pending operation... Insert a value.')
+    this._operation = type;
+  }
+
+  /**
+   * Get the result.
+   * @throws <i>Error</i> if an operation is pending.
+   */
+  public get result(): number
+  {
+    if (this._operation)
+      throw new Error('Pending operation... Insert a value.')
+    return this._value;
+  }
 }
--- src/Webpage.ts
+++ src/Webpage.ts
@@ -3,60 +3,60 @@ import * as request from 'request';
 
 export class Webpage
 {
-    /**
-     * Retrieve a webpage and save the content to a file.
-     * @param url The webpage to get.
-     * @param path The path to save the retrieved page.
-     * @throws <i>Error</i> if something went wrong getting teh webpage.
-     
-     */
-    public saveWebpage(url, path): Promise<string>
+  /**
+   * Retrieve a webpage and save the content to a file.
+   * @param url The webpage to get.
+   * @param path The path to save the retrieved page.
+   * @throws <i>Error</i> if something went wrong getting teh webpage.
+
+    */
+  public saveWebpage(url, path): Promise<string>
+  {
+    return this.getWebpage(url).then((content) => this._writeFile(path, content)).catch((error) =>
     {
-        return this.getWebpage(url).then((content) => this._writeFile(path, content)).catch((error) =>
-        {
-            throw new Error(error);
-        })
-    }
+      throw new Error(error);
+    })
+  }
 
-    /**
-     * Get the given url and return the body.
-     * It rejects if an error occurred or the response code is not 200.
-     * @param url The webpage to get.
-     */
-    public getWebpage(url): Promise<any>
+  /**
+   * Get the given url and return the body.
+   * It rejects if an error occurred or the response code is not 200.
+   * @param url The webpage to get.
+   */
+  public getWebpage(url): Promise<any>
+  {
+    return new Promise(function (resolve, reject)
     {
-        return new Promise(function (resolve, reject)
-        {
-            request.get(url, function (error, response, body)
-            {
-                if (error)
-                    return reject(error)
+      request.get(url, function (error, response, body)
+      {
+        if (error)
+          return reject(error)
 
-                if (response.statusCode !== 200)
-                    return reject(response)
+        if (response.statusCode !== 200)
+          return reject(response)
 
-                resolve(body)
-            })
-        })
-    }
+        resolve(body)
+      })
+    })
+  }
 
-    /**
-     * Create a file in the given path and write the given content into it.
-     * It rejects if an error occurred.
-     * @param path The path to be written.
-     * @param content The file content.
-     */
-    private _writeFile(path, content): Promise<string>
+  /**
+   * Create a file in the given path and write the given content into it.
+   * It rejects if an error occurred.
+   * @param path The path to be written.
+   * @param content The file content.
+   */
+  private _writeFile(path, content): Promise<string>
+  {
+    return new Promise<string>((resolve, reject) =>
     {
-        return new Promise<string>((resolve, reject) =>
-        {
-            fs.writeFile(path, content, (error) =>
-            {
-                if (error)
-                    return reject(error)
+      fs.writeFile(path, content, (error) =>
+      {
+        if (error)
+          return reject(error)
 
-                resolve(path)
-            })
-        })
-    }
+        resolve(path)
+      })
+    })
+  }
 }
