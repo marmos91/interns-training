2cbbeb6cd30bd69675fcd5b38e56c67cc5a71de8
Francesco Pigozzi <pigozzi.fr@gmail.com>
2019-10-11 19:58:06 +0200

    Add Client and Server logic

---
+++ src/Client.ts
@@ -0,0 +1,108 @@
+import {Socket, AddressInfo, createSocket} from "dgram";
+
+import {Address, IMessage, MessageType} from "./Interfaces";
+
+class Client
+{
+    private static _default_server: Address = {ip: 'localhost', port: 8000};
+    
+    private _socket: Socket;
+    private _server: Address = Client._default_server;
+
+    constructor(private _id: number, private _username: string) {
+        this.reset()
+    };
+
+    async connect(server?: Address): Promise <Address>
+    {
+        if(server)
+            this._server = server;
+
+        console.log(`Client(${this._id})::connect`, this._server);
+        
+        await this.send_message_to_server(this.create_message(MessageType.REGISTRATION));
+
+        return this._server
+    };
+
+    async disconnect(): Promise <any>
+    {
+        console.log(`Client(${this._id})::disconnect`);
+        
+        await this.send_message_to_server(this.create_message(MessageType.LEAVE));
+
+        return this.reset();
+    };
+
+    send(message: string, to: number): Promise <any>
+    {
+        console.log(`Client(${this._id})::disconnect`, message, to);
+
+        return this.send_message_to_server(this.create_message(MessageType.MESSAGE, message, to));
+    };
+
+    broadcast(message: string): Promise <any>
+    {
+        console.log(`Client(${this._id})::broadcast`, message);
+
+        return this.send_message_to_server(this.create_message(MessageType.BROADCAST, message));
+    };
+
+    private reset()
+    {
+        this._server = Client._default_server;
+        this._socket = createSocket('udp4');
+
+        this._socket.on('message', (msg: Buffer, rinfo: AddressInfo) =>
+        {
+            console.log(`Client(${this._id})::message`, this._id, msg.toString());
+        });
+    };
+
+    private create_message(type: MessageType, payload?: string, destination?: number): IMessage
+    {
+        return {
+            type,
+            source: {
+                id: this._id,
+                username: this._username,
+            },
+            destination,
+            payload,
+        };
+    };
+
+    private send_message_to_server(message: IMessage): Promise<any>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const msg = Client.serialize_message(message);
+            this._socket.send(
+                msg,
+                0,
+                msg.length,
+                this._server.port,
+                this._server.ip,
+                (error) => 
+                {
+                    console.log(`Client(${this._id})::send_message_to_server`, message, error);
+                    
+                    if(error)
+                        return reject(error);
+                    
+                    return resolve();
+                },
+            );
+        });
+    };
+
+    private static serialize_message(message: IMessage): string {
+        return JSON.stringify(message);
+    };
+
+    public static deserialize_message(message: Buffer): IMessage {
+        return JSON.parse(message.toString());
+    };
+}
+
+export default Client
---
+++ src/Server.ts
@@ -0,0 +1,117 @@
+import {SocketType, createSocket, Socket, AddressInfo} from "dgram";
+
+import {IClient, Address, MessageType, IMessage} from "./Interfaces";
+
+import Client from "./Client";
+
+type ServerListenCallback = (port: number) => void
+
+class Server
+{
+    private static _default_port: number = 8000;
+    
+    private _port: number = Server._default_port;
+    private _socket: Socket;
+    private _clients: {[id: number] : IClient} = {};
+
+    listen(port?: number | ServerListenCallback, callback?: ServerListenCallback)
+    {
+        this._port = port && typeof port === 'number' ? port : Server._default_port;
+
+        this._socket = createSocket('udp4')
+
+        this._socket.on('close', this.on_close.bind(this));
+
+        this._socket.on('error', this.on_error.bind(this));
+
+        this._socket.on('message', this.on_message.bind(this));
+
+        this._socket.on('listening', () =>
+        {
+            console.log('Server::listening', this._port);
+            
+            if(callback)
+                return callback(this._port)
+
+            if(port && typeof port === 'function')
+                return port(this._port)
+        });
+        
+        this._socket.bind(this._port);
+    };
+
+    shutdown(callback?)
+    {
+        this._clients = {};
+
+        if(this._socket) return this._socket.close(callback);
+
+        if(callback) return callback()
+    };
+
+    private on_close()
+    {
+        console.log('Server::closed');
+    }
+
+    private on_error(error)
+    {
+        console.error('Server::error', error);
+        this.shutdown();
+    }
+
+    private on_message(msg: Buffer, rinfo: AddressInfo)
+    {
+        const message: IMessage = Client.deserialize_message(msg);
+            const address: Address = {
+                ip: rinfo.address,
+                port: rinfo.port,
+            }
+            
+            console.log('Server::message', message, address);
+
+            switch (message.type) {
+                case MessageType.REGISTRATION:
+                {
+                    this._clients[message.source.id] = {
+                        id: message.source.id,
+                        username: message.source.username,
+                        address: {
+                            ip: address.ip,
+                            port: address.port,
+                        },
+                    };
+                    break;
+                }
+
+                case MessageType.LEAVE:
+                {
+                    delete this._clients[message.source.id];
+                    break;
+                }
+                
+                case MessageType.MESSAGE:
+                {
+                    const {ip, port} = this._clients[message.destination].address;
+                    this._socket.send(message.payload, 0, message.payload.length, port, ip);
+                    break;
+                }
+                
+                case MessageType.BROADCAST:
+                {
+                    const clients_to_send = Object.values(this._clients).filter(({id}) => id !== message.source.id)
+                    for(let client of clients_to_send)
+                    {
+                        const {ip, port} = client.address;
+                        this._socket.send(message.payload, 0, message.payload.length, port, ip)
+                    }
+                    break;
+                }
+                default:
+                    console.error(`Unknown message of type ${message.type}`);
+                    break;
+            }
+    }
+}
+
+export default Server;
