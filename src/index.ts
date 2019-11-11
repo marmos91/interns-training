179e7f68f37ecf0cc42359d6eac437c681de4f51
OmarSerrah <omarserrah@gmail.com>
2019-11-11 16:03:30 +0100

    SecondoLivello
    Secondo livello completato

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export class Challenge
+{
+public buzz(value :number)
+{
+  return new Promise((resolve, reject) =>
+{
+    if ((value % 3 === 0) && (value % 5 === 0))
+    {
+      return resolve('fizzbuzz');
+    }
+    else if (value % 3 == 0)
+    {
+      return resolve('fizz');
+    }
+    else if (value % 5 == 0)
+    {
+      return resolve('buzz');
+    }
+    else return resolve(value);
+  });
+
+}}
