63793a9ee72690b896d0bd77535b25defed90b1e
OmarSerrah <omarserrah@gmail.com>
2019-11-12 11:36:40 +0100

    Review codeStyle level1
    Modifiche codeStyle al codice del primo livello, tab, spaces e braces

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 export class Challenge
 {
-public buzz(value :number)
+    public buzz(value: number)
 {
     return new Promise((resolve, reject) =>
 {
     if ((value % 3 === 0) && (value % 5 === 0))
-    {
-      return resolve('fizzbuzz');
-    }
-    else if (value % 3 == 0)
-    {
-      return resolve('fizz');
-    }
-    else if (value % 5 == 0)
-    {
-      return resolve('buzz');
-    }
-    else return resolve(value);
+        return resolve('fizzbuzz');
+    else if (value % 3 === 0)
+        return resolve('fizz');
+    else if (value % 5 === 0)
+        return resolve('buzz');
+    else
+        return resolve(value);
 });
 
 }}
