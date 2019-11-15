90cb35a6e5dfdf07ea1cdfad20b264c2e584e643
OmarSerrah <omarserrah@gmail.com>
2019-11-15 09:57:26 +0100

    Indentation corrected
    Correzione dell'indentazione

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 export class Challenge
 {
     public buzz(value: number)
-{
-    return new Promise((resolve, reject) =>
-{
-    if ((value % 3 === 0) && (value % 5 === 0))
-        return resolve('fizzbuzz');
-    else if (value % 3 === 0)
-        return resolve('fizz');
-    else if (value % 5 === 0)
-        return resolve('buzz');
-    else
-        return resolve(value);
-});
-
-}}
+    {
+        return new Promise((resolve, reject) =>
+        {
+            if ((value % 3 === 0) && (value % 5 === 0)) 
+                return resolve('fizzbuzz');
+            else if (value % 3 === 0)  
+                return resolve('fizz');
+            else if (value % 5 === 0) 
+                return resolve('buzz');
+            else 
+                return resolve(value);
+        });
+    }
+}
