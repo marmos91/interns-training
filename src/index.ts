23e8622ad8cd4c99959d5fadb30eab92a33da9c2
Jacopo <bonta.jacopo@gmail.com>
2019-12-10 17:17:54 +0100

    solved styling issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-export class Challenge {
-    buzz(x: number):Promise<string | number> {
-        return new Promise((resolve, reject) => {
-            if (x % 3 === 0 && x % 5 === 0) return resolve('fizzbuzz')
-            if (x % 3 === 0) return resolve('fizz')
-            if (x % 5 === 0) return resolve('buzz')
-            return resolve(x)
-        })
+export class Challenge
+{
+    buzz(x: number): Promise<string | number>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            if (x % 3 === 0 && x % 5 === 0)
+                return resolve('fizzbuzz');
+            if (x % 3 === 0)
+                return resolve('fizz');
+            if (x % 5 === 0)
+                return resolve('buzz');
+            return resolve(x);
+        });
     }
-}
+};
