d170223db3d673556dca361addf53c725169fce8
Jacopo <bonta.jacopo@gmail.com>
2019-12-11 16:04:14 +0100

    add a test to ensure that getWebpage and _writeFile are called only once

--- test/Webpage.test.ts
+++ test/Webpage.test.ts
@@ -1,6 +1,8 @@
 import {expect} from 'chai'
 import {Webpage} from '../src/Webpage'
+import {stub} from 'sinon';
 import * as ServerMock from 'mock-http-server'
+import * as fs from 'fs';
 
 describe('Level 2 - Unit', () =>
 {
@@ -8,14 +10,10 @@ describe('Level 2 - Unit', () =>
     {
         describe('getWebpage method', () =>
         {
-            let webpage;
+            let webpage = new Webpage();
             let server = new ServerMock({ host: 'localhost', port: 9000 });
 
-            beforeEach((done) => {
-                webpage = new Webpage()
-                server.start(done)
-            });
-            afterEach((done) => server.stop(done));
+            before((done) => server.start(done));
 
             it('should resolve to page body', (done) =>
             {
@@ -50,10 +48,41 @@ describe('Level 2 - Unit', () =>
                 });
 
                 webpage.getWebpage('http://localhost:9000/test')
-                    .catch(error =>
-                        {
-                            done();
-                        });
+                    .catch(error => done());
+            });
+
+            after((done) => server.stop(done));
+        });
+
+        describe('saveWebpage method', () =>
+        {
+            let get_webpage, write_file, webpage
+                , url = 'http://localhost:9000/foo'
+                , path = 'my/cool/path'
+                , content = 'some content';
+
+            beforeEach(() =>
+            {
+                webpage = new Webpage();
+                get_webpage = stub(webpage, "getWebpage").resolves(content);
+                write_file = stub(webpage, "_writeFile").resolves(path);
+            });
+
+            afterEach(() =>
+            {
+                get_webpage.restore();
+                write_file.restore();
+            });
+
+            it('should call getWebpage and _writeFile once', (done) =>
+            {
+                webpage.saveWebpage(url, path)
+                    .then(() =>
+                    {
+                        expect(get_webpage.calledOnceWith(url)).to.be.true;
+                        expect(write_file.calledOnceWith(path, content)).to.be.true;
+                        done();
+                    });
             });
         })
     });
