e4af62bdfc3a45ea6b8ddd3c24726ce46651c743
Jacopo <bonta.jacopo@gmail.com>
2019-12-12 16:55:09 +0100

    add error handling implement broadcast method

--- src/Server.ts
+++ src/Server.ts
@@ -26,18 +26,22 @@ export default class Server
         this._port = typeof port === 'number' ? port : 8000;
         const cb = typeof port === 'function'? port : callback;
 
+        this._socket.on('error', (err) =>
+        {
+            throw new Error(err.message);
+        });
+        
         this._socket.bind(this._port);
 
         if (cb)
-            this._socket.on('listening', cb);
+            this._socket.on('listening', () => cb(this._port));
         
         this._socket.on('message', (msg, rinfo) =>
         {
             const message = <IMessage>JSON.parse(msg.toString());
-            const client = this._clients[message.source.id];
 
-            if (!client && message.type !== MessageType.REGISTRATION)
-                this.sendMessage('You must send a registration message first in order to interact with the server', {ip: rinfo.address, port: rinfo.port});
+            if (!this._clients[message.source.id] && message.type !== MessageType.REGISTRATION)
+                this._socket.send('You must send a registration message first in order to interact with the server', rinfo.port, rinfo.address);
 
             switch(message.type)
             {
@@ -45,13 +49,13 @@ export default class Server
                     this.registerClient(message.source.id, message.source.username, {ip: rinfo.address, port: rinfo.port});
                     break;
                 case MessageType.LEAVE:
-                    this.removeClient(client.id);
+                    this.removeClient(message.source.id);
                     break;
                 case MessageType.MESSAGE:
-                    this.sendMessage(message.payload, client.address);
+                    this.sendMessage(message.payload, message.destination);
                     break;
                 case MessageType.BROADCAST:
-                    this.broadcastMessage(message.payload);
+                    this.broadcastMessage(message.payload, message.source.id);
                     break;
                 default:
                     throw new Error('invalid message type');
@@ -98,21 +102,23 @@ export default class Server
     /**
      * broadcast a message to connected clients
      * @param message the message to be broadcasted
+     * @param from the sender client id
      */
-    private broadcastMessage(message: string): void
+    private broadcastMessage(message: string, from: number): void
     {
-        // this._socket.setBroadcast(true); unsure of what is does
-        this._socket.send(message, this._port);
+        for(const client of Object.values(this._clients))
+            if (client.id !== from)
+                this.sendMessage(message, client.id);
     }
 
     /**
      * send a message to a client
      * @param message the message to send
-     * @param address the client address
+     * @param to the client id where the message will be sent
      */
-    private sendMessage(message: string, address: Address): void
+    private sendMessage(message: string, to: number): void
     {
-        // this._socket.setBroadcast(false); unsure but maybe it needs
-        this._socket.send(message, address.port, address.ip);
+        const destination_address = this._clients[to].address;
+        this._socket.send(message, destination_address.port, destination_address.ip);
     }
 }
