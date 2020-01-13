36b10b93775b04dec584344b602e940205ef50da
Luca Merighi <luca.merighi93@gmail.com>
2020-01-13 11:58:39 +0100

    Code style fixes

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ export class Challenge
 {
     buzz(num: number): Promise<string | number>
     {
-        let res: string | number = "";
-        if (num % 3 === 0) res += "fizz";
-        if (num % 5 === 0) res += "buzz";
-        if (res.length === 0) res = num;
-        return new Promise((resolve, reject) => resolve(res));
+        let response: string | number = "";
+
+        if(num % 3 === 0)
+            response += "fizz";
+
+        if(num % 5 === 0)
+            response += "buzz";
+
+        if(response.length === 0)
+            response = num;
+
+        return new Promise((resolve, reject) => resolve(response));
     }
 }
