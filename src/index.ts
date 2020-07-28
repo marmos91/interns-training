26c00a7937890f836e65ecbe66dcf2776832cbae
Nicola Serra <nicola@MacBook-Pro.local>
2020-07-28 22:03:43 +0200

    Esercizio completato

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export class Challenge
+{
+    public buzz(n: number): Promise<string|number>
+    {
+        let output: string = "";
+
+        return new Promise<string|number>((resolve, reject) =>
+        {
+            if(n%3 == 0)
+                output += "fizz";
+            if(n%5 == 0)
+                output += "buzz";
+            else if(n%3 != 0)
+                resolve(n);
+            
+            resolve(output);
+        });
+    }
+}
