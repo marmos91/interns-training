bbe37d75e12265da141b6361f326c7bf99178e9c
Nicola Serra <nicola@MacBook-Pro.local>
2020-07-30 17:10:32 +0200

    Completed - Tested saveWebpage()

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -8,41 +8,41 @@ n2 = 13;
 
 describe("Calculator", () =>
 {
-  beforeEach(() => calc = new Calculator());
+	beforeEach(() => calc = new Calculator());
 
-  test("Addition", () =>
-  {
-    calc.input(n1);
-    calc.operation(Operation.ADD);
-    calc.input(n2);
-    
-    expect(calc.result).toBe(n1 + n2);
-  });
+	test.each([[1, 2, 3], [4, -5, -1]])("Addition", (a, b, r) =>
+	{
+		calc.input(a);
+		calc.operation(Operation.ADD);
+		calc.input(b);
 
-  test("Subtraction", () =>
-  {
-    calc.input(n1);
-    calc.operation(Operation.SUB);
-    calc.input(n2);
-    
-    expect(calc.result).toBe(n1 - n2);
-  });
+		expect(calc.result).toBe(r);
+	});
+
+	test("Subtraction", () =>
+	{
+		calc.input(n1);
+		calc.operation(Operation.SUB);
+		calc.input(n2);
+
+		expect(calc.result).toBe(n1 - n2);
+	});
   
-  test("Moltiplication", () =>
-  {
-    calc.input(n1);
-    calc.operation(Operation.MUL);
-    calc.input(n2);
-    
-    expect(calc.result).toBe(n1 * n2);
-  });
+	test("Moltiplication", () =>
+	{
+		calc.input(n1);
+		calc.operation(Operation.MUL);
+		calc.input(n2);
 
-  test("Division", () =>
-  {
-    calc.input(n1);
-    calc.operation(Operation.DIV);
-    calc.input(n2);
-    
-    expect(calc.result).toBe(n1 / n2);
-  });
+		expect(calc.result).toBe(n1 * n2);
+	});
+
+	test("Division", () =>
+	{
+		calc.input(n1);
+		calc.operation(Operation.DIV);
+		calc.input(n2);
+
+		expect(calc.result).toBe(n1 / n2);
+	});
 });
--- test/web.test.ts
+++ test/web.test.ts
@@ -1,32 +1,49 @@
 import {Webpage} from "../src/Webpage";
 import * as Server from "mock-http-server"; 
-import { doesNotThrow } from "assert";
+import * as fs from "fs";
 
 var web: Webpage = new Webpage;
 var server: Server = new Server(
 {
-  host: "localhost",
-  port: 9000
+	host: "localhost",
+	port: 9000
 });
 var url: string = "http://localhost:9000/resource";
 
 describe("Webpage", () =>
 {
-  beforeEach((done) => server.start(done));
-  afterEach((done) => server.stop(done));
+	beforeEach((done) => server.start(done));
+	afterEach((done) => server.stop(done));
 
-  test("Reciving a page", async () =>
-  {
-    server.on(
-    {
-      path: '/resource',
-      reply:
-      {
-        body: "Hello World!"
-      }
-    });
-    let page = await web.getWebpage(url);
+	test("Reciving a page", async () =>
+	{
+		server.on(
+		{
+			path: "/resource",
+			reply:
+			{
+				body: "Hello World!"
+			}
+		});
+		let page = await web.getWebpage(url);
 
-    expect(page).toBe("Hello World!");
-  });
+		expect(page).toBe("Hello World!");
+	});
+
+	test("Saveing a page", async () =>
+	{
+		server.on(
+		{
+			path: "/resource",
+			reply:
+			{
+				body: "Another Hello World!"
+			}
+		});
+		await web.saveWebpage(url, "./mypage");
+
+		let page: string = fs.readFileSync("./mypage", "utf8");
+
+		expect(page).toBe("Another Hello World!");
+	});
 });
