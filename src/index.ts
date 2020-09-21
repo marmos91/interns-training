3a3be76307ffc824f76f19f7516974b4f41f3750
Maxim <maxim.kalinin@cubbit.io>
2020-09-21 17:31:17 +0300

    fix: code style

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-class Challenge {
-    buzz(input: number): Promise<string | number> {
-        return new Promise(resolve => resolve(`${input % 3 === 0 ? "fizz" : ""}${input % 5 === 0 ? "buzz" : ""}` || input));
+class Challenge 
+{
+    buzz(input: number): Promise<string | number> 
+    {
+        return new Promise(resolve => 
+            resolve(`${input % 3 === 0 ? "fizz" : ""}${input % 5 === 0 ? "buzz" : ""}` || input)
+        );
     }
 }
 
