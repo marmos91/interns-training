18e474394995818add32aad5f0c5dd708bdce437
Marko <markopyeshchyk@gmail.com>
2021-04-02 10:22:52 +0200

    corrected style errors

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 export class Challenge
 {
-    public buzz(n: number) :Promise<any> 
+    public buzz(n: number) :Promise<String | number> 
     {
-        var result: string;
+        let result = '';
 
-        result = '';
         if(n % 3 == 0)
             result = 'fizz';
+
         if(n % 5 == 0)
             result += 'buzz';
 
-        var p = new Promise((resolve, reject) => 
+        return new Promise((resolve, reject) => 
         {
             if (result != '')
-                resolve(result);
+                return resolve(result);
             else resolve(n);
         });
-
-        return p;
     }
 }
