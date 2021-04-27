67c4b974cb6343901989f73a599728602ce10a71
Bacco <65162638+TommyBacco@users.noreply.github.com>
2021-04-27 16:17:17 +0200

    Update src/index.ts
    Co-authored-by: lorenzo cazzoli <lurenz.sgp@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ export class Challenge
         return new Promise((resolve, reject) =>
         {
             if(a % 3 === 0)
+            {
                 if(a % 5 === 0)
                     resolve('fizzbuzz');
                 else 
                     resolve('fizz');
+            }
             else if(a % 5 === 0)
                  resolve('buzz');
             else 
