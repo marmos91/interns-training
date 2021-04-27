7523a2ac01bc5b71f350b20152e5a7359e8427a3
TommyBacco <bacco@autistici.org>
2021-04-27 15:57:14 +0200

    typescript project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export class Challenge
+{
+    public buzz<Promise>(a: number)
+    {
+        return new Promise((resolve, reject) =>
+        {
+            if(a % 3 === 0)
+                if(a % 5 === 0)
+                    resolve('fizzbuzz');
+                else 
+                    resolve('fizz');
+            else if(a % 5 === 0)
+                 resolve('buzz');
+            else 
+                resolve(a); 
+        });
+    }
+}
