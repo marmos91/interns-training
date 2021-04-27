20243341b54d7247a49ef91377dd3ba8a8ddddfd
TommyBacco <bacco@autistici.org>
2021-04-27 12:51:12 +0200

    hello world function

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+
+function say(name)
+{
+    if(name === undefined)
+    {
+        return("Hello world!");
+    }
+    else
+    {
+        return("Hello " + name + "!");
+    }
+}
+
+module.exports = { say };
