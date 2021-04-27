be835a40c57b7d89b0c255d009300b53dbd9a260
TommyBacco <bacco@autistici.org>
2021-04-27 16:46:05 +0200

    snake case and spaces adjusted

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 export class Challenge
 {
-    public buzz<Promise>(myNumber: number)
+    public buzz<Promise>(my_number: number)
     {
         return new Promise((resolve, reject) =>
         {
-            var myString: string = '';
+            let my_string: string = '';
 
-            if(myNumber % 3 === 0)
-                myString = 'fizz';
-            if(myNumber % 5 === 0)
-                myString += 'buzz';
-            if(myString === '')
-                resolve(myNumber);
+            if(my_number % 3 === 0)
+                my_string = 'fizz';
+
+            if(my_number % 5 === 0)
+                my_string += 'buzz';
+
+            if(my_string === '')
+                resolve(my_number);
             else    
-                resolve(myString);
+                resolve(my_string);
         });
     }
 }
