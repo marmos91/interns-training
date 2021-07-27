7d5d150a26a0c519370412d18a6ea75a533e6e1a
Simone Bembi <simone.bembi@gmail.com>
2021-07-27 15:59:11 +0200

    feat: implemented server class

---
+++ src/Server.ts
@@ -0,0 +1,165 @@
+import { IClient, IMessage, MessageType } from './Interfaces';
+import * as dgram from 'dgram';
+
+export default class Server
+{
+    private _port: number = 8000;
+    private _socket: dgram.SocketType = 'udp4';
+    private _clients: {
+        [id: number]: IClient;
+    } = {};
+
+    private _server: dgram.Socket;
+
+    constructor()
+    {
+        this._setup_server();
+    }
+
+    listen(port?: number): void;
+    listen(callback?: (port: number) => void): void;
+    listen(port: number, callback?: (port: number) => void): void;
+    listen(port_or_callback?: number | ((port: number) => void), callback?: (port: number) => void): void
+    {
+        let cb: typeof callback;
+        if (port_or_callback) {
+            if (typeof port_or_callback === 'function') {
+                cb = port_or_callback;
+            } else if (typeof port_or_callback === 'number') {
+                this._port = port_or_callback;
+                cb = callback;
+            }
+        }
+
+        if (cb)
+        {
+            this._server.on('listening', () =>
+            {
+                cb(this._port);
+            });
+        }
+
+        this._server.bind(this._port);
+    }
+
+    shutdown(callback?: () => void): void {
+        this._server.close(() =>
+        {
+            this._setup_server();
+            if (callback)
+                callback();
+        });
+    }
+
+    private _setup_server()
+    {
+        this._server = dgram.createSocket(this._socket);
+        this._server.on('error', this._on_server_error.bind(this));
+        this._server.on('message', this._on_server_message.bind(this));
+    }
+
+    private _on_server_error(error: Error): void
+    {
+        console.log(`Server error:\n ${error.stack}`);
+        this._server.close();
+    }
+
+    private _on_server_message(message: Buffer, remote_info: dgram.RemoteInfo): void
+    {
+        let parsed_message: IMessage;
+        try
+        {
+            parsed_message = JSON.parse(message.toString());
+        } catch
+        {
+            console.error(`Could not parse message ${message}. Not JSON.`)
+        }
+        try
+        {
+            switch (parsed_message.type)
+            {
+                case MessageType.REGISTRATION:
+                    {
+                        const id = this._get_id_from_message(parsed_message);
+                        const username = this._get_username_from_message(parsed_message);
+                        
+                        const client: IClient = {
+                            address: {
+                                ip: remote_info.address,
+                                port: remote_info.port
+                            },
+                            id,
+                            username,
+                        };
+                        this._clients[id] = client;
+                    }
+                    break;
+                case MessageType.LEAVE:
+                    {
+                        const id = this._get_id_from_message(parsed_message);
+                        if (!(id in this._clients))
+                            throw new Error(`Client not registered.`);
+                        delete this._clients[id];
+                    }
+                    break;
+                case MessageType.MESSAGE:
+                    {
+                        const destination_id = this._get_destination_id_from_message(parsed_message);
+                        const payload = this._get_payload_from_message(parsed_message);
+
+                        const client = this._clients[destination_id];
+                        if (!client)
+                            throw new Error(`Destination not registered.`);
+
+                        this._server.send(payload, client.address.port, client.address.ip);
+                    }
+                    break;
+                case MessageType.BROADCAST:
+                    {
+                        const payload = this._get_payload_from_message(parsed_message);
+
+                        for (const client of Object.values(this._clients))
+                        {
+                            this._server.send(payload, client.address.port, client.address.ip);
+                        }
+                    }
+                    break;
+            }
+        } catch (err)
+        {
+            console.error(`Could not parse message ${message}. Protocol error: \n${err}`);
+        }
+    }
+
+    private _get_id_from_message(message: IMessage): number
+    {
+        const id = message.source?.id;
+        if (id === undefined)
+            throw new Error(`source.id not defined`);
+        return id;
+    }
+
+    private _get_username_from_message(message: IMessage): string
+    {
+        const username = message.source?.username;
+        if (username === undefined)
+            throw new Error(`source.username not defined`);
+        return username;
+    }
+
+    private _get_destination_id_from_message(message: IMessage): number
+    {
+        const id = message.destination;
+        if (id === undefined)
+            throw new Error(`destination not defined`);
+        return id;
+    }
+
+    private _get_payload_from_message(message: IMessage): string
+    {
+        const payload = message.payload;
+        if (payload === undefined)
+            throw new Error(`payload not defined`);
+        return payload;
+    }
+}
