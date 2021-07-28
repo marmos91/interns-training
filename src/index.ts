b400183f311b7e8085a30d3ceb4a69690da604d4
Simone Bembi <simone.bembi@gmail.com>
2021-07-28 10:57:46 +0200

    style: allman_style and spacing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-export class Challenge {
-    buzz(value: number): Promise<string | number> {
+export class Challenge
+{
+    buzz(value: number): Promise<string | number>
+    {
         const multipleOf3 = value % 3 === 0;
         const multipleOf5 = value % 5 === 0;
+
         if (multipleOf3 && multipleOf5)
             return Promise.resolve('fizzbuzz');
+
         if (multipleOf3)
             return Promise.resolve('fizz');
+
         if (multipleOf5)
             return Promise.resolve('buzz');
+            
         return Promise.resolve(value);
     }
 }
