dc710805b504e23daa5801294eeda627e21a0ca6
Waldemar Zielinski <waldek99zielinski@gmail.com>
2021-08-24 14:10:51 +0200

    class Challenge

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export class Challenge
+{
+    public buzz(input: number): Promise<string | number>
+    {
+        let output = '';
+
+        return new Promise((resolve, reject) => 
+        {
+            if(input % 3 === 0)
+                output += 'fizz';
+
+            if(input % 5 === 0)
+                output += 'buzz';
+            
+            if(output)
+                resolve(output);
+            else
+                resolve(input);
+        });
+    }
+}
