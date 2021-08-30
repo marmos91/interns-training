19a953fa8d16c74dbafb65bcdfbcb4ecaacd74ec
Waldemar Zielinski <waldek99zielinski@gmail.com>
2021-08-30 08:45:54 +0200

    feat: ui

--- resources/index.js
+++ resources/index.js
@@ -1,30 +1,11 @@
-document.getElementById('incrementCount').addEventListener('click', () => {
-    console.log('renderer:incrementCount:click')
-    bridgeAPI.incrementCount()
-})
+document.getElementById('button-select-file').addEventListener('click', () => bridgeAPI.load_file());
 
-document.getElementById('decrementCount').addEventListener('click', () => {
-    console.log('renderer:decrementCout:click')
-    bridgeAPI.decrementCount()
-})
-
-document.getElementById('button-select-file').addEventListener('click', () => 
+document.getElementById('button-send').addEventListener('click', (event) => 
 {
-    console.log('index: button select file');
-    bridgeAPI.load_file();
-});
-
-document.getElementById('button-send').addEventListener('click', () => 
-{
-    console.log('index: button send');
     const ip = document.getElementById('input-ip');
     const port = document.getElementById('input-port');
-    console.log("ip: ",ip.value," port: ", port.value);
+    console.log('index.js: ', ip.value, port.value);
     bridgeAPI.send_file(ip.value, port.value);
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('renderer:DOMContentLoaded')
-    bridgeAPI.updateCountDisplay();
-    bridgeAPI.get_server_port();
-})
+document.addEventListener('DOMContentLoaded', () =>  bridgeAPI.get_server_port());
